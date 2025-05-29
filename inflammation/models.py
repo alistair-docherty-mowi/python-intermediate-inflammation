5497b23aced1f0815b9aa262c07d67f05ee3b2f9
Alistair <alistair.docherty@mowi.com>
2025-05-29 13:11:11 +0100

    removed white space

--- inflammation/models.py
+++ inflammation/models.py
@@ -12,7 +12,7 @@ als change for develop branch
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -33,4 +33,4 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2D inflammation data array."""
     return np.min(data, axis=0)
-
+    
